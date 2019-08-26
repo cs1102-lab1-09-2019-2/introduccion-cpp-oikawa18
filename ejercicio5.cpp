bc1b88d985e7c687c6a40d28a843ffc52d14e3c0
oikawa18 <54276784+oikawa18@users.noreply.github.com>
2019-08-25 21:31:47 -0500

    Completado..

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-using std::cout;
+#include <vector>
+using namespace std;
 
 int main() {
 
     vector<int> a {1, 2, 3, 4, 5};
     for (auto i : a) {
-        // Imprimir los numeros
+      cout<<i<<"\n";
+      
+
     }
+        // Imprimir los numeros
+    
 }
